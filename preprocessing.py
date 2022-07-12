6c174bd82ad41fd9a64bafc7f0b8cbfc5a21660d
KwonYujin <rnjsdb72@gmail.com>
2022-07-13 01:14:02 +0900

    add try/except in preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -9,18 +9,27 @@ import librosa
 
 def mp3_to_mel(n_fft = 2048, n_mels = 256, hop_length = 512,
                win_length = 2048):
+    error = 0
+    error_lst = []
     files = sorted(glob('./download/*.mp3'))
     if not os.path.exists('./download/mel_spec'):
         os.makedirs('./download/mel_spec')
     print('Start Converting!')
     for file in tqdm(files):
-        sig, sr = librosa.load(file)
-        mel_spec = librosa.feature.melspectrogram(sig, sr=sr, n_fft=n_fft, n_mels=n_mels, 
-                                            hop_length=hop_length, win_length=win_length)
-        log_mel_spec = librosa.power_to_db(mel_spec)
-        np.save('./download/mel_spec/'+file.split('/')[-1].split('.')[0]+'.npy',
-            log_mel_spec)
+        try:
+            sig, sr = librosa.load(file)
+            mel_spec = librosa.feature.melspectrogram(sig, sr=sr, n_fft=n_fft, n_mels=n_mels, 
+                                                hop_length=hop_length, win_length=win_length)
+            log_mel_spec = librosa.power_to_db(mel_spec)
+            np.save('./download/mel_spec/'+file.split('/')[-1].split('.')[0]+'.npy',
+                log_mel_spec)
+        except:
+            error +=1
+            error_lst.append(file)
     print('Convert Complete!')
+    print('Number of Error: ', error)
+    print('Error List:')
+    print(error_lst)
 
 if __name__ == '__main__':
     mp3_to_mel()
