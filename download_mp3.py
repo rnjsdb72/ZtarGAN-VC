710d5d99c9969bbab7c71752d1af615018ff6d99
KwonYujin <rnjsdb72@gmail.com>
2022-07-13 17:38:44 +0900

    fix code effectively

--- download_mp3.py
+++ download_mp3.py
@@ -9,13 +9,15 @@ import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome
 
-def download_mp3(channel):
+def download_mp3(channel, start=0, end=-1):
     c = Channel(channel)
 
     df_lst = []
     print('Start Download')
     videos = c.videos
-    for video in tqdm(videos):
+    if end == -1:
+        end = len(videos)
+    for video in tqdm(videos[start:end]):
         error = 0
         try:
             video.streams.filter(only_audio=True).first().download('./download/')
@@ -46,14 +48,8 @@ def crawling_lyrics():
     df = pd.read_csv('./download/df_music.csv')
 
     print('\tPreprocessing...')
-
-    pl_1 = list(Playlist('https://www.youtube.com/playlist?list=PL5rkMpxC5Ex9T89yMwWTTl38OF6NMxrT7'))
-    pl_2 = list(Playlist('https://www.youtube.com/watch?v=IPtyDLwMyJg&list=PL5rkMpxC5Ex9SZ5fbVBm0WhlA48J5C-He'))
-    pl_3 = list(Playlist('https://www.youtube.com/watch?v=22qlOevg4KY&list=PL5rkMpxC5Ex_Dyg-PGwKGPSjZ80tHQC3l'))
-
-    except_lst = pl_1 + pl_2 + pl_3
-
-    df = df.query('url not in @except_lst').reset_index(drop=True)
+    
+    df = df.title.map(lambda x: x.startswith('[TJ노래방')).reset_index(drop=True)
     df['title2'] = df.title.map(lambda x: x.split(']')[1].split('/')[0].split('TJ')[0].strip())
 
     dir_driver = chromedriver_autoinstaller.install()
@@ -74,13 +70,17 @@ def crawling_lyrics():
             lyrics.append('')
             error += 1
     df['lyrics'] = lyrics
-    df.to_csv(f'./download/df_music.csv', index=False)
-    print(f'\tDownload "df_music.csv" Success!')
+    df.to_csv(f'./download/df_music_with_lyrics.csv', index=False)
+    print(f'\tDownload "df_music_with_lyrics.csv" Success!')
     print(f'Error Rate:\t {error}/{df.shape[0]}')
 
 if __name__ == '__main__':
-    url = input('Enter the URL: \t')
-    download_mp3(url)
+    url = input('Enter the URL:\t')
+    point = input('Insert start point/end point(ex. 0/-1):\t')
+    start, end = point.split('/')
+    start, end = int(start), int(end)
+    
+    download_mp3(url, start, end)
     convert()
     print('Ready to Crawling Lyrics...')
     crawling_lyrics()
--- preprocessing.py
+++ preprocessing.py
@@ -7,41 +7,24 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings(action='ignore')
 import librosa
-import torch
-from torch.nn.utils.rnn import pad_sequence
 
-def mp3_to_mel(n_fft = 2048, n_mels = 256, hop_length = 512,
-               win_length = 2048):
+def mp3_to_mel(n_fft = 2048, n_mels = 256, hop_length = 512, win_length = 2048):
     error = 0
     error_lst = []
-    res_lst = []
-    title_lst = []
     files = sorted(glob('./download/*.mp3'))
     print('Start Converting!')
-    for idx, file in enumerate(tqdm(files)):
+    if not os.path.exists('./data'):
+        os.makedirs('./data')
+    for file in tqdm(files):
         try:
             sig, sr = librosa.load(file)
             mel_spec = librosa.feature.melspectrogram(sig, sr=sr, n_fft=n_fft, n_mels=n_mels, 
                                                 hop_length=hop_length, win_length=win_length)
             log_mel_spec = librosa.power_to_db(mel_spec)
-            res_lst.append(torch.tensor(log_mel_spec))
-            title_lst.append(file.split('/')[-1].split('.')[0])
+            np.save(f"./data/{file.split('/')[-1].split('.')[0]}.npy", log_mel_spec)
         except:
             error +=1
             error_lst.append(file)
-        if len(res_lst) == 5000:
-            num_data = len(res_lst)
-            lst = []
-            for res in res_lst:
-                lst += [*torch.tensor(res)]
-            res = pad_sequence(lst, batch_first=True).view(num_data,256,-1)
-            if not os.path.exists('./data'):
-                os.makedirs('./data')
-            torch.save(res, f'./data/dataset_audio{idx}.pt')
-            pd.DataFrame({'title':title_lst}).to_csv(f'./data/titles{idx}.csv', index=False)
-            res_lst = []
-            title_lst = []
-            lst = []
     print('Convert Complete!')
     print('Number of Error: ', error)
     print('Error List:')
@@ -49,3 +32,4 @@ def mp3_to_mel(n_fft = 2048, n_mels = 256, hop_length = 512,
 
 if __name__ == '__main__':
     mp3_to_mel()
+
