dcb622e161eaa99c08033fc6bfd1bf8ae634c163
KwonYujin <rnjsdb72@gmail.com>
2022-07-14 11:36:16 +0900

    change library librosa to torch "preprocessing.py"

--- preprocessing.py
+++ preprocessing.py
@@ -1,35 +1,53 @@
 import os
 from glob import glob
 from tqdm import tqdm
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import warnings
-warnings.filterwarnings(action='ignore')
-import librosa
 
-def mp3_to_mel(n_fft = 2048, n_mels = 256, hop_length = 512, win_length = 2048):
-    error = 0
-    error_lst = []
-    files = sorted(glob('./download/*.mp3'))
-    print('Start Converting!')
-    if not os.path.exists('./data'):
-        os.makedirs('./data')
-    for file in tqdm(files):
-        try:
-            sig, sr = librosa.load(file)
-            mel_spec = librosa.feature.melspectrogram(sig, sr=sr, n_fft=n_fft, n_mels=n_mels, 
-                                                hop_length=hop_length, win_length=win_length)
-            log_mel_spec = librosa.power_to_db(mel_spec)
-            np.save(f"./data/{file.split('/')[-1].split('.')[0]}.npy", log_mel_spec)
-        except:
-            error +=1
-            error_lst.append(file)
-    print('Convert Complete!')
-    print('Number of Error: ', error)
-    print('Error List:')
-    print(error_lst)
+import torch
+import torch.nn as nn
+import torchaudio
+import torchaudio.transforms as AT
+from torch.nn.utils.rnn import pad_sequence
+
+class mp3_to_mel():
+    def __init__(self, sr:int = 44100, n_fft:int = 2048, n_mels:int = 128,
+                 hop_length:int = 512, win_length:int = 2048):
+        self.n_mels = n_mels
+        self.to_mel = nn.Sequential(
+                                AT.MelSpectrogram(
+                                    sample_rate = sr,
+                                    n_fft = n_fft,
+                                    win_length = win_length,
+                                    hop_length = hop_length,
+                                    n_mels = n_mels
+                                ),
+                                AT.AmplitudeToDB()
+                            )
+        
+    def load_signals(self, files):
+        c = torchaudio.load(files[0])[0].shape[0]
+        
+        print('Loading Audio...')
+        sigs = list(map(lambda x: torchaudio.load(x)[0], files))
+        
+        lst = []
+        for sig in tqdm(sigs):
+            lst += [*sig]
+        
+        print('Start to padding...')
+        self.sig_padded = pad_sequence(lst, batch_first=True).view(len(files), 2, -1)
+        
+        print('Complete!')
+        
+    def convert_to_mel(self):
+        mels = self.to_mel(self.sig_padded)
+        return mels
 
 if __name__ == '__main__':
-    mp3_to_mel()
+    files = glob('./download/*.mp3')
+    converter = mp3_to_mel()
+    converter.load_signals(files)
+    mels = converter.convert_to_mel()
+    torch.save(mels, './data/data_audio.pt')
+    print('Save Success!')
 
