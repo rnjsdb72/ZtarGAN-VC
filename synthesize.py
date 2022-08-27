cbc133d2a0ee06b467214bcbb26ff4015026b5ca
KwonYujin <rnjsdb72@gmail.com>
2022-08-28 01:45:30 +0900

    fit synthesize.py as my code

--- synthesize.py
+++ synthesize.py
@@ -1,7 +1,6 @@
 import re
 from string import punctuation
 import json
-from collections import namedtuple
 
 import torch
 import numpy as np
@@ -13,6 +12,7 @@ from utils.model import get_model, get_vocoder
 from utils.tools import to_device, synth_samples, arg_parse
 from dataset import TextDataset
 from text import text_to_sequence
+from text.korean import tokenize, normalize_nonchar
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -56,23 +56,20 @@ def preprocess_english(text, preprocess_config):
     return np.array(sequence)
 
 
-def preprocess_mandarin(text, preprocess_config):
+def preprocess_korean(text, preprocess_config):
     lexicon = read_lexicon(preprocess_config["path"]["lexicon_path"])
 
     phones = []
-    pinyins = [
-        p[0]
-        for p in pinyin(
-            text, style=Style.TONE3, strict=False, neutral_tone_with_five=True
-        )
-    ]
-    for p in pinyins:
-        if p in lexicon:
-            phones += lexicon[p]
+    words = filter(None, re.split(r"([,;.\-\?\!\s+])", text))
+    for w in words:
+        if w in lexicon:
+            phones += lexicon[w]
         else:
-            phones.append("sp")
+            phones += list(filter(lambda p: p != " ", tokenize(w, norm=False)))
+    phones = "{" + "}{".join(phones) + "}"
+    phones = normalize_nonchar(phones, inference=True)
+    phones = phones.replace("}{", " ")
 
-    phones = "{" + " ".join(phones) + "}"
     print("Raw Text Sequence: {}".format(text))
     print("Phoneme Sequence: {}".format(phones))
     sequence = np.array(
@@ -111,45 +108,45 @@ def synthesize(model, step, configs, vocoder, batchs, control_values):
 if __name__ == "__main__":
     args = arg_parse()
     with open(args.cfg, 'r') as f:
-        cfgs = json.load(f, object_hook=lambda d: namedtuple('x', d.keys())(*d.values()))
+        cfgs = json.load(f)
 
     # Check source texts
-    if cfgs.mode == "batch":
-        assert cfgs.source is not None and cfgs.text is None
-    if cfgs.mode == "single":
-        assert cfgs.source is None and cfgs.text is not None
+    if cfgs['mode'] == "batch":
+        assert cfgs['source'] is not None and cfgs['text'] is None
+    if cfgs['mode'] == "single":
+        assert cfgs['source'] is None and cfgs['text'] is not None
 
     # Read Config
-    preprocess_config = cfgs.preprocess
-    model_config = cfgs.model
-    train_config = cfgs.train
+    preprocess_config = cfgs['preprocess']
+    model_config = cfgs['model']
+    train_config = cfgs['train']
     configs = (preprocess_config, model_config, train_config)
 
     # Get model
-    model = get_model(args, configs, device, train=False)
+    model = get_model(cfgs, configs, device, train=False)
 
     # Load vocoder
     vocoder = get_vocoder(model_config, device)
 
     # Preprocess texts
-    if cfgs.mode == "batch":
+    if cfgs['mode'] == "batch":
         # Get dataset
-        dataset = TextDataset(cfgs.source, preprocess_config)
+        dataset = TextDataset(cfgs['source'], preprocess_config)
         batchs = DataLoader(
             dataset,
             batch_size=8,
             collate_fn=dataset.collate_fn,
         )
-    if cfgs.mode == "single":
-        ids = raw_texts = [cfgs.text[:100]]
-        speakers = np.array([cfgs.speaker_id])
-        if preprocess_config["preprocessing"]["text"]["language"] == "en":
-            texts = np.array([preprocess_english(cfgs.text, preprocess_config)])
-        elif preprocess_config["preprocessing"]["text"]["language"] == "zh":
-            texts = np.array([preprocess_mandarin(cfgs.text, preprocess_config)])
+    if cfgs['mode'] == "single":
+        ids = raw_texts = [cfgs['text'][:100]]
+        speakers = np.array([cfgs['speaker_id']])
+        if preprocess_config["preprocessing"]["text"]["language"] == "kr":
+            texts = np.array([preprocess_korean(cfgs['text'], preprocess_config)])
+        elif preprocess_config["preprocessing"]["text"]["language"] == "en":
+            texts = np.array([preprocess_english(cfgs['text'], preprocess_config)])
         text_lens = np.array([len(texts[0])])
         batchs = [(ids, raw_texts, speakers, texts, text_lens, max(text_lens))]
 
-    control_values = cfgs.pitch_control, cfgs.energy_control, cfgs.duration_control
+    control_values = cfgs['pitch_control'], cfgs['energy_control'], cfgs['duration_control']
 
-    synthesize(model, cfgs.restore_step, configs, vocoder, batchs, control_values)
+    synthesize(model, cfgs['restore_step'], configs, vocoder, batchs, control_values)
