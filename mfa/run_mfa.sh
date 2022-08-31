dadd631239da1b5694f7c5072e53abb14f6f0ff9
KwonYujin <rnjsdb72@gmail.com>
2022-08-31 22:24:14 +0900

    fix run_mfa.sh variable problem

--- mfa/run_mfa.sh
+++ mfa/run_mfa.sh
@@ -1,4 +1,4 @@
-DIR=$1
+DIR=kss
 mfa train_g2p ../lexicon/p_lexicon.txt korean.zip
 mfa g2p korean.zip ../raw_data/${DIR} korean.txt
 mfa train ../raw_data/${DIR} korean.txt ../preprocessed_data/${DIR}/TextGrid
