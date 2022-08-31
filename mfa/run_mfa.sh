f5204606c1b74dde9b24999ba0608f08aec8f161
KwonYujin <rnjsdb72@gmail.com>
2022-09-01 01:57:32 +0900

    fix run_mfa.sh to run

--- mfa/run_mfa.sh
+++ mfa/run_mfa.sh
@@ -1,4 +1,2 @@
 DIR=kss
-mfa train_g2p ../lexicon/p_lexicon.txt korean.zip
-mfa g2p korean.zip ../raw_data/${DIR} korean.txt
-mfa train ../raw_data/${DIR} korean.txt ../preprocessed_data/${DIR}/TextGrid
+mfa train ../raw_data/${DIR} ../lexicon/p_lexicon.txt ../preprocessed_data/${DIR}/TextGrid
