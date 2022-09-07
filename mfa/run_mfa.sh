309a46168bb1e8e0f96c5b6055909fe312b76536
KwonYujin <rnjsdb72@gmail.com>
2022-09-08 04:56:41 +0900

    add train g2p code in run_mfa.sh

--- mfa/run_mfa.sh
+++ mfa/run_mfa.sh
@@ -1,2 +1,4 @@
 DIR=kss
-mfa train ../raw_data/${DIR} ../lexicon/p_lexicon.txt ../preprocessed_data/${DIR}/TextGrid
+mfa train_g2p --clean ../lexicon/p_lexicon.txt korean.zip
+mfa g2p --clean korean.zip ../raw_data/${DIR} korean.txt
+mfa train --clean ../raw_data/${DIR} korean.txt ../preprocessed_data/${DIR}/TextGrid
