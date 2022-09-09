6bc7d24578163ae956534afbcb3df0b7a02e1728
KwonYujin <rnjsdb72@gmail.com>
2022-09-10 02:18:14 +0900

    return run_mfa.sh - delete g2p_train

--- mfa/run_mfa.sh
+++ mfa/run_mfa.sh
@@ -1,4 +1,2 @@
 DIR=kss
-mfa train_g2p --clean ../lexicon/p_lexicon.txt korean.zip
-mfa g2p --clean korean.zip ../raw_data/${DIR} korean.txt
-mfa train --clean ../raw_data/${DIR} korean.txt ../preprocessed_data/${DIR}/TextGrid
+mfa train --clean ../raw_data/${DIR} ../lexicon/p_lexicon.txt ../preprocessed_data/${DIR}/TextGrid
